4d390c02854651f40a395ff46953e0d0aff6a242
h13rbard <gerar80ti@gmail.com>
2026-06-10 14:25:15 -0400

    fix: correct resource group reference typo in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-odoo-prod"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group_by_name.rg.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
