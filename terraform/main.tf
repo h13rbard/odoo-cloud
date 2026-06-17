ceafc2a85bec3abbc0fb8963343f8689fe4d2860
rocky Cloud User <azureuser@vm-odoo-rockylinux9.d5qd22hrgscuvkshnu1qzatmgc.cphx.internal.cloudapp.net>
2026-06-17 20:26:09 +0000

    Add Prometheus,loki and promtail

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,6 +110,18 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+ # Regla de Seguridad para Monitoreo
+  security_rule {
+    name                       = "Prometheus-Scrape"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9100"
+    source_address_prefix      = "91.134.143.28" # 👈 Solo tu VPS puede medir tu servidor
+    destination_address_prefix = "*"
+  }
 # 6. INTERFAZ DE RED (NIC) Y ASOCIACIÓN DEL NSG
 resource "azurerm_network_interface" "nic" {
   name                = "nic-odoo-prod"
